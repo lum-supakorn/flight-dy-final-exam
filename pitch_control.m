c2a65a504a3ab2ff8e168b34058cae997a13ac06
Lum <supakorn.s@protonmail.com>
2020-05-13 00:18:58 +0700

    Add goals to pitch control

--- pitch_control.m
+++ pitch_control.m
@@ -1,15 +1,15 @@
 % Pitch control
 
-ele_input = deg2rad(11);
+ele_input = deg2rad(1);
 
 % Open-loop step response
-t = 0:0.01:400;
-step(ele_input * alpha_eta, t);
-ylabel('Pitch angle {rad}');
-title('Open-loop step response');
+t = 0:0.01:1000;
+% step(ele_input * theta_eta, t);
+% ylabel('Pitch angle {rad}');
+% title('Open-loop step response');
 
 % Closed-loop step response
-sys_cl = feedback(alpha_eta, 1);
+sys_cl = feedback(theta_eta, 0.001);
 step(ele_input * sys_cl, t);
 ylabel('Pitch angle {rad}');
 title('Closed-loop step response');
