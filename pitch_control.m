5044eaf2e6e1334b2f51fd17ee95a377783931ae
Lum <supakorn.s@protonmail.com>
2020-05-15 17:54:36 +0700

    Finalize sims

--- pitch_control.m
+++ pitch_control.m
@@ -2,15 +2,15 @@
 % Pitch damper
 
 % Open-loop step response
-t = 0:0.01:200;
+t = 0:0.01:10;
 step(q_eta * deg2rad(1), t);
 hold on;
 ylabel('Pitch angle {rad}');
 title('Open-loop step response');
 
 % Closed-loop step response
-sys_cl = feedback(q_eta * deg2rad(1), -7.2);
-step(sys_cl, t);
+sys_cl = feedback(q_eta, -7.2);
+step(sys_cl * deg2rad(1), t);
 ylabel('Pitch angle {rad}');
 title('Closed-loop step response');
 % 
